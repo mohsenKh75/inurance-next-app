ec3ece47975ac1cb51c4ae223573d3cee2411a14
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-01-09 21:54:58 +0330

    feat: move to register if not logged in

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -1,4 +1,6 @@
-import React, { ReactElement } from "react";
+import { useUser } from "@/hooks/useUser";
+import { useRouter } from "next/navigation";
+import { ReactElement, Suspense, useEffect } from "react";
 
 interface Props {
   children: ReactElement | Array<ReactElement>;
@@ -6,11 +8,20 @@ interface Props {
   footer?: ReactElement;
 }
 export function MainLayout({ children, header, footer }: Props) {
+  const { isLoggedIn } = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.replace("/register");
+    }
+  }, []);
   return (
-    <div className="h-full flex flex-col">
-      <header>{header}</header>
-      <main className="flex flex-col h-full w-full">{children}</main>
-      <footer>{footer}</footer>
-    </div>
+    <Suspense>
+      <div className="h-full flex flex-col">
+        <header>{header}</header>
+        <main className="flex flex-col h-full w-full">{children}</main>
+        <footer>{footer}</footer>
+      </div>
+    </Suspense>
   );
 }
