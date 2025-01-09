233169d9a91d01c7439a2f5323f2ad56da07e9b0
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-01-09 12:52:38 +0330

    feat: create register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { MainLayout } from "../MainLayout";
+import { FormProvider, useForm } from "react-hook-form";
+
+import { PageTitle } from "@/components/shared/PageTitle";
+import { Input } from "@/components/shared/Input";
+import { Button } from "@/components/shared/Button";
+
+export default function Register() {
+  const methods = useForm<FormData>();
+
+  return (
+    <MainLayout>
+      <div className="px-5">
+        <PageTitle className="py-5" title="ثبت نام" />
+        <FormProvider {...methods}>
+          <form className="flex w-full flex-col items-center gap-5 ">
+            <Input name="firstName" type="text" placeholder="نام" />
+            <Input name="lastName" type="text" placeholder="نام خانوادگی" />
+            <Input name="password" type="password" placeholder="رمز عبور" />
+            <Button type="submit">ثبت نام</Button>
+          </form>
+        </FormProvider>
+      </div>
+    </MainLayout>
+  );
+}
