d23179f6241a2293ebf32e256f87b91f1533b1b7
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-01-09 16:30:42 +0330

    feat: wrap provider around app

---
+++ src/providers/ReduxProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import store from "@/store/user/store";
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+
+function ReduxProvider({ children }: { children: ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
+
+export default ReduxProvider;
