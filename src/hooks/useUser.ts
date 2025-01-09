cdc8b020fe911f219ae4d4027925dbc2ce799821
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-01-09 21:59:37 +0330

    feat: add logout action to hook

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,8 +1,10 @@
 import { UserSelector } from "@/store/types";
-import { useSelector } from "react-redux";
+import { logout } from "@/store/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export function useUser() {
+  const dispatch = useDispatch();
   const { user, isLoggedIn } = useSelector((state: UserSelector) => state.auth);
 
-  return { user, isLoggedIn };
+  return { user, isLoggedIn, logout: () => dispatch(logout()) };
 }
